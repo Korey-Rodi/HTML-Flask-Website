5071cc59b4de0a92eb16855cc00fd1e44fb6762c
Korey Rodi <rodikorey@gmail.com>
2025-05-07 14:54:30 -0400

    Website Pages

--- website/app.py
+++ website/app.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 def Welcome():
     return render_template("index.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def SignUp():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        data = open("UserLog.txt", "w")
+        data.write("username: " + username + "password: " + password)
+        return redirect(url_for("ThankYou"))
+    
     return render_template('SignUpPage.html')
+
+@app.route("/thankyou")
+def ThankYou():
+    return render_template("ThankYouPage.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
